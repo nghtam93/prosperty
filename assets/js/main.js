9035b99bdf1f249b18b6864ed36ea8b804fe451f
doannguyen263 <doannguyen263@gmail.com>
2022-01-20 13:34:33 +0700

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -147,13 +147,33 @@ $(document).ready(function(){
         $(this).parent().addClass("sub-active"),
         $(this).parent().find('.sub-menu').first().slideToggle()
     })
-
-
-
     //check home
     if($('body').hasClass( "home" )){
 
     }
+
+
+    // Tooltip page account
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+      return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+
+    // ClipboardJS
+    if($(document).find('.js-item-copy').length !=0){
+        let clipboard = new ClipboardJS('.js-item-copy');
+        clipboard.on('success', function (e) {
+            let trigger_button = e.trigger;
+            // update the tooltip title, get the tooltip instance, and show it
+            trigger_button.setAttribute('data-bs-original-title', 'Copied!');
+            let btn_tooltip = bootstrap.Tooltip.getInstance(trigger_button);
+            btn_tooltip.show();
+            // reset the tooltip title
+            trigger_button.setAttribute('data-bs-original-title', 'Copy to clipboard');
+        });
+    }
+
+
 });
 
 
